219e0f28217e196c16feca5a06d50e4fd40cbbfd
ottoak <ottoak41@gmail.com>
2023-05-12 22:39:59 -0400

    added ansi esc codes

---
+++ src/ansi_esc_codes.rs
@@ -0,0 +1,55 @@
+pub enum EscCode {
+    // Core escape character \x1b[
+    Esc,
+    // Text formatting
+    Bold,
+    Italic,
+    Underline,
+    // Colors
+    Black,
+    Red,
+    Green,
+    Yellow,
+    Blue,
+    Magenta,
+    Cyan,
+    White,
+}
+
+pub fn format(code: &EscCode) -> String {
+    match code {
+        EscCode::Esc => "\x1b[".into(),
+        EscCode::Bold => "1".into(),
+        EscCode::Italic => "3".into(),
+        EscCode::Underline => "4".into(),
+        EscCode::Black => "30".into(),
+        EscCode::Red => "31".into(),
+        EscCode::Green => "32".into(),
+        EscCode::Yellow => "33".into(),
+        EscCode::Blue => "34".into(),
+        EscCode::Magenta => "35".into(),
+        EscCode::Cyan => "36".into(),
+        EscCode::White => "37".into(),
+    }
+}
+
+pub fn build_format(codes: Vec<EscCode>) -> String {
+    let fmt_str = codes
+        .iter()
+        .map(|c| format(c))
+        .collect::<Vec<String>>()
+        .join(";");
+
+    format(&EscCode::Esc) + &fmt_str + "m"
+}
+
+#[test]
+fn test_build_format() {
+    let fmts = vec![EscCode::Bold, EscCode::Underline, EscCode::Red];
+
+    let fmt_str = &build_format(fmts);
+
+    let expected = "\x1b[1;4;31m";
+
+    assert_eq!(fmt_str, expected);
+}
--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,14 @@
 
 mod utils;
 
+mod ansi_esc_codes;
+pub use ansi_esc_codes::{build_format, EscCode};
+
 // TODO: finalize trait
 trait Table {
+    fn new_table(headers: Vec<&str>, conf: Option<Config>) -> TableData;
     fn add_row(&mut self, row_data: Vec<String>);
+    fn set_header_format(&mut self, fmt_str: String);
 
     fn calculate_widths(&self);
     fn print_header(&self);
@@ -14,12 +19,12 @@ trait Table {
 }
 
 // TODO: finalize struct
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct TableData {
     padding: usize,
     headers: Vec<String>,
     header_fmt: String,
-    //col_widths: Vec<usize>,
+    col_widths: Vec<usize>,
     rows: Vec<Vec<String>>,
 }
 
@@ -30,27 +35,33 @@ struct Config {
 
 // TODO
 // https://stackoverflow.com/questions/28951503/how-can-i-create-a-function-with-a-variable-number-of-arguments
-impl TableData {
-    fn new(headers: Vec<&str>, conf: Option<Config>) -> TableData {
+impl Table for TableData {
+    fn new_table(headers: Vec<&str>, conf: Option<Config>) -> TableData {
         match conf {
             Some(_conf) => {
                 todo!()
             }
             None => {
                 TableData {
-                    headers: parse_args(headers),
+                    headers: parse_args(&headers),
                     padding: 2,
                     header_fmt: String::from(""), //\x1b[1m
+                    col_widths: headers
+                        .iter()
+                        .map(|h| h.len() + 2)
+                        .collect::<Vec<usize>>(),
                     rows: Vec::new(),
                 }
             }
         }
     }
-}
 
-impl Table for TableData {
     fn add_row(&mut self, row_data: Vec<String>) {
         self.rows.push(row_data)
+    }
+
+    fn set_header_format(&mut self, fmt_str: String) {
+        self.header_fmt = fmt_str;
     }
 
     // TODO: implement
@@ -64,14 +75,14 @@ impl Table for TableData {
 
     // TODO: complete
     fn represent(&self) -> String {
-        let rep = format!(
+        let header_rep = format!(
             "{}{}{}",
             self.header_fmt,
             self.headers.join(&" ".repeat(self.padding)),
             String::from("") // "\x1b[0m" for bold
         );
 
-        rep
+        header_rep
     }
 
     // TODO: complete
@@ -79,13 +90,13 @@ impl Table for TableData {
 }
 
 // TODO: replace?
-fn parse_args(v: Vec<&str>) -> Vec<String> {
+fn parse_args(v: &Vec<&str>) -> Vec<String> {
     v.iter().map(|x| x.to_string()).collect::<Vec<String>>()
 }
 
 fn main() {
     let headers = vec!["A", "B", "C"];
-    let t = TableData::new(headers, None);
+    let t = TableData::new_table(headers, None);
 
     let table_rep = t.represent();
 
@@ -99,17 +110,28 @@ mod tests {
     #[test]
     fn test_header() {
         let headers = vec!["A", "B", "C"];
-        let t = TableData::new(headers, None);
+        let t = TableData::new_table(headers, None);
         let table_rep = t.represent();
 
         let expected = String::from("A  B  C");
 
         assert_eq!(table_rep, expected);
     }
+
+    #[test]
+    fn test_col_widths_no_rows() {
+        let headers = vec!["dog", "capybara", "panda"];
+        let t = TableData::new_table(headers, None);
+
+        let expected_widths = vec![5, 10, 7];
+
+        assert_eq!(t.col_widths, expected_widths);
+    }
+
     #[test]
     fn test_one_row() {
         let headers = vec!["A", "B", "C"];
-        let mut t = TableData::new(headers, None);
+        let mut t = TableData::new_table(headers, None);
         t.add_row(row!("cat", "dog", "bird"));
 
         let table_rep = t.represent();
@@ -127,7 +149,7 @@ cat  dog  bird
     #[test]
     fn test_two_rows() {
         let headers = vec!["A", "B", "C"];
-        let mut t = TableData::new(headers, None);
+        let mut t = TableData::new_table(headers, None);
         t.add_row(row!("cat", "dog", "bird"));
         t.add_row(row!("bird", "dog", "cat"));
 
